12474d0e37dcaaf2da9fbe67a806dfa6b307bb2e
MrMishka02 <m.gogia@hotmail.com>
2023-08-31 00:25:52 +0400

    docs: added SQL base

---
+++ python/hogwarts.py
@@ -0,0 +1,12 @@
+import csv
+from cs50 import SQL
+
+db = SQL("sqlite:///Hogwarts.db")
+
+house = input("House: ")
+
+rows = db.execute("SELECT COUNT(*) FROM Hogwarts WHERE House LIKE ?",
+                  "%" + house + "%")
+row = rows[0]
+
+print(row["COUNT(*)"])
